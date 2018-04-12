f913362fca1fa3abd8152474ba84fafa22c026c0
Evan <2523815778@qq.com>
2018-04-12 19:48:51 +0800

    Problem 617

---
+++ 617MergeTwoBinaryTrees.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is not None and t2 is not None:
+            t1.val += t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            return t1
+        else:
+            return t1 if t1 is not None else t2
