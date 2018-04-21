cc4e6d019e87e9887f4c7b5a009921c6f80a1b78
Evan <2523815778@qq.com>
2018-04-21 21:39:55 +0800

    Problem 206

---
+++ 206ReverseLinkedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        curr = head
+        while curr:
+            latt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = latt
+        return prev
