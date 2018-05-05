0f21cc8de43a5af68a545df0da4b2523aaccbd19
Evan <2523815778@qq.com>
2018-05-05 22:31:27 +0800

    AddSolution 300

--- 300LongestIncreasingSubsequence.py
+++ 300LongestIncreasingSubsequence.py
@@ -4,6 +4,26 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        size, dp = 0, [0] * len(nums)
+        for n in nums:
+            left, right = 0, size
+            while left < right:  # Algorithm: Binary Search
+                mid = (left + right) // 2
+                if dp[mid] >= n:
+                    right = mid
+                else:
+                    left = mid + 1
+            dp[left] = n
+            size = max(left + 1, size)
+        return size
+
+
+class SolutionB:
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         dp = [0] * len(nums)
         res = 0
         for i in range(len(nums)):
